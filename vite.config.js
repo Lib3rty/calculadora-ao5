7cc42b21a9ae539df32968d9d256f13e67c1e61d
Lib3rty <TU_CORREO_DE_GITHUB@ejemplo.com>
2026-09-12 18:34:39 -0400

    Configurar GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/calculadora-ao5/',
   plugins: [react()],
   server: {
     port: 5173,
